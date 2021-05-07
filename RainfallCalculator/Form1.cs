3db2173a5060455f0748e316b9d18a28f92ec828
Robert Koruna <rkoruna@student.cscc.edu>
2021-05-07 16:51:37 -0400

    first commit

---
+++ RainfallCalculator/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace RainfallCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            string filename = openFileDialog1.FileName;
+            string readfile = File.ReadAllText(filename);
+
+            StreamReader sr = new StreamReader(filename);
+           
+       
+  
+
+
+
+        }
+    }
+}
