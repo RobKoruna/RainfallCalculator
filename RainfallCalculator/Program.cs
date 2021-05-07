c74bd5d7f20fe853a779c0a7185708ad323fef4a
tjkoruna <tkoruna@gmail.com>
2021-05-07 17:09:44 -0400

    Test commit

--- RainfallCalculator/Program.cs
+++ RainfallCalculator/Program.cs
@@ -8,6 +8,7 @@ namespace RainfallCalculator
 {
     static class Program
     {
+        /// Test - does this push to the git repository?
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
